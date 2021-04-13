d8e248a677354a2e4f85cf7b35b2c632feb0e8f6
hoonpig <hoonpig@gmail.com>
2021-04-13 15:48:40 +0900

    0413_study

---
+++ rpa_basic/2_desktop/3_mouse_action.py
@@ -0,0 +1,12 @@
+import pyautogui
+
+pyautogui.sleep(3)
+#print(pyautogui.position())
+#pyautogui.click(1968,23,duration=1)
+
+#pyautogui.mouseDown()
+#pyautogui.mouseUp()
+
+#pyautogui.click(clicks=500) 마우스 클릭 수지정
+#pyautogui.drag() #드래그
+#pyautogui.scroll() # 스크롤 : 양수는 위쪽, 음수는 아래쪽
